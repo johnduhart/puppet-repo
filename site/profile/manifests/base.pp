2662b6a598a9db13fad0b08238ebefddc0753fb5
John Du Hart <john@johnduhart.me>
2015-01-27 21:03:57 -0500

    Add proxy override for oracle java

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,4 +1,14 @@
 class profile::base {
 	include ::ntp
 	include ::apt
+
+	file { '10oracle-proxy':
+		ensure  => present,
+		path    => "${apt::params::apt_conf_d}/10oracle-proxy",
+		content => "Acquire::http::Proxy { download.oracle.com DIRECT; };",
+		notify  => Exec['apt_update'],
+		mode    => '0644',
+		owner   => root,
+		group   => root,
+	}
 }
