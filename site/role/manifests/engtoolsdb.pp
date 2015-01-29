385a9d76512ef25a64827ba6c418d6f991eff6ab
John Du Hart <john@johnduhart.me>
2015-01-29 11:30:59 -0500

    Add db config to db node

--- site/role/manifests/engtoolsdb.pp
+++ site/role/manifests/engtoolsdb.pp
@@ -1,3 +1,18 @@
 class role::engtoolsdb {
-	
+	include profile::base
+	include ::lvm
+
+	lvm::logical_volume { 'db':
+		volume_group => 'datavg',
+		size => '20G',
+		mountpath => '/mnt/db',
+	} ->
+	class { 'postgresql::globals':
+		manage_package_repo => true,
+		version             => '9.3',
+	} ->
+	class { 'postgresql::server': } ->
+	postgresql::server::tablespace { 'toolsts':
+		location => '/mnt/db',
+	}
 }
